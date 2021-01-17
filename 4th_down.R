b607a445771ba36629afe4d5abbcb71a6023634b
Jared Lee <13jaredlee@gmail.com>
2021-01-17 16:41:31 -0700

    split up code

--- 4th_down.R
+++ 4th_down.R
@@ -2,32 +2,44 @@ cfbscrapR:::wp_model
 
 
 tibble(ExpScoreDiff = 0,TimeSecsRem = 1800,half = 2,
-       ExpScoreDiff_Time_Ratio = 0,Under_two = FALSE,pos_team_timeouts_rem_before = 3,
-       def_pos_team_timeouts_rem_before = 3) %>% 
+       ExpScoreDiff_Time_Ratio = 0,Under_two = FALSE, pos_team_timeouts_rem_before = 3,
+       def_pos_team_timeouts_rem_before = 3) %>%
   predict(object = cfbscrapR:::wp_model,newdata = .,type = "response")
 cfbscrapR:::ep_model
 tibble(TimeSecsRem = 1800,yards_to_goal = 75,down = "1",log_ydstogo = log(yards_to_goal),
-       Goal_To_Go = FALSE, pos_score_diff_start = 0,Under_two = FALSE) %>% 
+       Goal_To_Go = FALSE, pos_score_diff_start = 0,Under_two = FALSE) %>%
   predict(object = cfbscrapR:::ep_model,newdata = .,type = "probs")
 cfbscrapR:::fg_model
 
 
-game_state <- tibble(yards_to_goal = 10, down = "1",distance = 10,pos_score = 7,def_pos_score = 0,
-                     TimeSecsRem = 1800,half = 2,pos_team_timeouts_rem_before = 3,
-                     def_pos_team_timeouts_rem_before = 3) %>% 
-  mutate(Under_two = TimeSecsRem < 120,
+game_state <- tibble(yards_to_goal = 47, down = "4",distance = 3,pos_score = 7,def_pos_score = 14,
+                     TimeSecsRem = 23,half = 1,pos_team_timeouts_rem_before = 3,
+                     def_pos_team_timeouts_rem_before = 3) %>%
+  mutate(Under_two = TimeSecsRem <= 120,
          log_ydstogo = log(yards_to_goal),
+         adj_TimeSecsRem = TimeSecsRem + ifelse(half == 1,1800,0),
          Goal_To_Go = distance == yards_to_goal,
          pos_score_diff_start = pos_score-def_pos_score,ep = NA)
 
 game_state$ep <- sum(predict(object = cfbscrapR:::ep_model,newdata = game_state,type = "probs")*c(0,3,-3,2,-7,-2,7) )
 
-game_state_ep <- game_state %>% 
+game_state_ep <- game_state %>%
   mutate(ExpScoreDiff = pos_score_diff_start + ep,
-         ExpScoreDiff_Time_Ratio = ExpScoreDiff/TimeSecsRem,
+         ExpScoreDiff_Time_Ratio = ExpScoreDiff/(adj_TimeSecsRem+1),
          wp = NA)
-game_state_ep$wp <- predict(object = cfbscrapR:::wp_model,newdata = game_state_ep,type = "response")  
+game_state_ep$wp <- predict(object = cfbscrapR:::wp_model,newdata = game_state_ep,type = "response")
 
 seasons <- 2014:2020
 pbp_all <- readRDS("pbp_all.RDS")
 pbp_4th_go <- pbp_4th %>% filter(rush+pass == 1)
+
+df <- cfb_game_info(2020,week = 16, team = "Rutgers")
+
+
+plays <- get_data(df)
+plays <- plays %>%
+  mutate(id_play = play_id)
+test <- cfbscrapR::create_epa(plays)
+
+
+pbp$boxscore
--- bot_test.R
+++ bot_test.R
@@ -1,25 +1,56 @@
+library(cfbscrapR)
+library(gt)
 library(MASS)
 library(rtweet)
-source("bot_functions.R")
-source("helpers.R")
+library(tidyverse)
+source("R/bot_functions.R")
+source("R/helpers.R")
+
+options(dplyr.summarise.inform = FALSE)
 #source("bot.R")
-
-df <- cfb_game_info(2020,week = 14, team = "Utah")
-plays <- get_data(df)
-for (i in 1:nrow(plays)) {
-
-  plays %>%
-    slice(i) %>%
-    tweet_play()
-  print(Sys.time())
-  Sys.sleep(120)
+team_info <- cfb_team_info()
+all_games <- cfb_game_info(2020,week = 1, season_type = "postseason") #week 15 done
+all_games <- all_games %>%
+  filter(home_team %in% team_info$school,
+         away_team %in% team_info$school) %>%
+  filter(!is.na(home_post_win_prob))# %>%
+# no pbp info for week 3 Duke. Week 6 USF.
+  #filter(home_team != "Florida Atlantic")
+old_plays <- readRDS("data/old_plays_all.RDS")
+for (game_row in 1:nrow(all_games)) {
+  game <- all_games[game_row,]
+  plays <- get_data(game)
+  message(glue::glue("{game$game_id} \nHome: {game$home_team}"))
+  for (i in 1:nrow(plays)) {
+    temp <- plays %>%
+      slice(i) %>%
+      make_tidy_data(punt_df)
+    old_plays <- bind_rows(old_plays,temp)
+  }
 }
+saveRDS(old_plays,"data/old_plays_all.RDS")
 
-old_plays <- tibble()
-for (i in 1:nrow(plays)) {
-  temp <- plays %>%
-    slice(i) %>%
-    make_tidy_data(punt_df)
-  old_plays <- bind_rows(old_plays,temp)
-}
-view(old_plays)
+# df <- cfb_game_info(2020,week = 16, team = "Rutgers")
+# plays <- get_data(df)
+# for (i in 1:nrow(plays)) {
+#
+#   plays %>%
+#     slice(i) %>%
+#     tweet_play()
+#   print(Sys.time())
+#   Sys.sleep(120)
+# }
+#
+# old_plays <- tibble()
+# for (i in 1:nrow(plays)) {
+#   temp <- plays %>%
+#     slice(i) %>%
+#     make_tidy_data(punt_df)
+#   old_plays <- bind_rows(old_plays,temp)
+# }
+# view(old_plays)
+#
+# old_plays2 <- map_df(1:nrow(plays),function(x) {
+#   plays[x,] %>% make_tidy_data(punt_df)
+# })
+# saveRDS(old_plays,"old_plays.RDS")
